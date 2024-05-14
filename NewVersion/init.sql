7975ebb99c546a928418e4ff9c7874386f467a1f
Lei Yang <lei.yang@student.uliege.be>
2024-05-14 14:26:18 +0200

    .

--- NewVersion/init.sql
+++ NewVersion/init.sql
@@ -1,28 +1,3 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-CREATE TABLE IF NOT EXISTS movie(
-    show_id VARCHAR(127) NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    description_film VARCHAR(4095) NOT NULL,
-    duration INT NOT NULL,
-    release_date INT NOT NULL,
-    date_added VARCHAR(127)NOT NULL,
-
-    PRIMARY KEY (show_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS show(
-    show_id VARCHAR(127) NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    description_film VARCHAR(4095) NOT NULL,
-    duration INT NOT NULL,
-    release_date INT NOT NULL,
-    date_added VARCHAR(127)NOT NULL,
-
-    PRIMARY KEY (show_id),
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 CREATE TABLE IF NOT EXISTS person(
@@ -31,48 +6,50 @@ CREATE TABLE IF NOT EXISTS person(
     lastname VARCHAR(255) NULL,
 
     PRIMARY KEY (person_id)
-
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 CREATE TABLE IF NOT EXISTS country(
     country_name VARCHAR(255) NOT NULL,
     country_id INT NOT NULL,
-
     PRIMARY KEY (country_id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 CREATE TABLE IF NOT EXISTS rating(
     rating_name VARCHAR(255) NOT NULL,
     rating_id INT NOT NULL,
 
     PRIMARY KEY (rating_id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE IF NOT EXISTS genre(
-    genre_name VARCHAR(255) NOT NULL,
-    genre_id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS show(
+    show_id VARCHAR(127) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description_film VARCHAR(4095) NOT NULL,
+    type_show VARCHAR(255) NOT NULL,
+    duration INT NOT NULL,
+    release_date INT NOT NULL,
+    date_added VARCHAR(127)NOT NULL,
 
-    PRIMARY KEY (genre_id)
-
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    PRIMARY KEY (show_id)
+); 
 
 CREATE TABLE IF NOT EXISTS rated(
     show_id VARCHAR(127) NOT NULL,
     rating_id INT NOT NULL,
-
     PRIMARY KEY (show_id, rating_id),
     FOREIGN KEY (show_id) REFERENCES show(show_id),
     FOREIGN KEY (rating_id) REFERENCES rating(rating_id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 CREATE TABLE IF NOT EXISTS filmed_in(
+
     show_id VARCHAR(127) NOT NULL,
     country_id INT NOT NULL,
 
     PRIMARY KEY (show_id, country_id),
     FOREIGN KEY (show_id) REFERENCES show(show_id),
     FOREIGN KEY (country_id) REFERENCES country(country_id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 CREATE TABLE IF NOT EXISTS directed_by(
     show_id VARCHAR(127) NOT NULL,
@@ -81,7 +58,7 @@ CREATE TABLE IF NOT EXISTS directed_by(
     PRIMARY KEY (show_id, person_id),
     FOREIGN KEY (show_id) REFERENCES show(show_id),
     FOREIGN KEY (person_id) REFERENCES person(person_id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 CREATE TABLE IF NOT EXISTS acted_in(
     show_id VARCHAR(127) NOT NULL,
@@ -90,7 +67,15 @@ CREATE TABLE IF NOT EXISTS acted_in(
     PRIMARY KEY (show_id, person_id),
     FOREIGN KEY (show_id) REFERENCES show(show_id),
     FOREIGN KEY (person_id) REFERENCES person(person_id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
+
+CREATE TABLE IF NOT EXISTS genre(
+    genre_name VARCHAR(255) NOT NULL,
+    genre_id INT NOT NULL,
+
+    PRIMARY KEY (genre_id)
+
+);
 
 CREATE TABLE IF NOT EXISTS genresof(
     show_id VARCHAR(127) NOT NULL,
@@ -99,6 +84,4 @@ CREATE TABLE IF NOT EXISTS genresof(
     PRIMARY KEY (show_id, genre_id),
     FOREIGN KEY (show_id) REFERENCES show(show_id),
     FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-"Loading to database here after the creating "
+);
